7322b3c733763c9f772b0cbb66fe7f905b28cabb
narigon-luna <124718528+narigon-luna@users.noreply.github.com>
2023-03-04 10:39:35 -0300

    Add files via upload

---
+++ Proyecto Final/js/funciones.js
@@ -0,0 +1,6 @@
+document.getElementById("boton_color").addEventListener("click", function(){
+    document.body.style.backgroundColor = "#B3FFF2";
+});
+document.getElementById("boton_default").addEventListener("click", function(){
+document.body.style.backgroundColor = "#919485";
+});
